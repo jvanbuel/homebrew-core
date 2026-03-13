5694b184ac34274a2b15a6b02749d714d27296c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-13 23:49:18 +0000

    wartremover 3.5.6

--- Formula/w/wartremover.rb
+++ Formula/w/wartremover.rb
@@ -1,8 +1,8 @@
 class Wartremover < Formula
   desc "Flexible Scala code linting tool"
   homepage "https://github.com/wartremover/wartremover"
-  url "https://github.com/wartremover/wartremover/archive/refs/tags/v3.5.5.tar.gz"
-  sha256 "d86414600757bb99ea94870e8bd419bdd8f6586292ec5243425caaf56579c515"
+  url "https://github.com/wartremover/wartremover/archive/refs/tags/v3.5.6.tar.gz"
+  sha256 "b40f76f3a14642f483c6be4ba1ed54882d1364a65481f4f892eb7900fd9c7d67"
   license "Apache-2.0"
   head "https://github.com/wartremover/wartremover.git", branch: "master"
 
